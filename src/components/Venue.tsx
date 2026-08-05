7f9a3774ca3802aec4e3106c34b110173819871f
Suman Mishra <suman132j@gmail.com>
2026-08-05 15:45:14 +0530

    venue fixed

--- src/components/Venue.tsx
+++ src/components/Venue.tsx
@@ -110,9 +110,9 @@ export default function Venue() {
             {/* Replace the src below with your actual Google Maps embed URL */}
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d69450.39437544129!2d88.08286280357282!3d22.653786618437454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f87ebfb6cf63bf%3A0x63185c9e3175a4a6!2sSovarani%20Memorial%20College!5e0!3m2!1sen!2sin!4v1785924609678!5m2!1sen!2sin"
-              width="600"
-              height="450"
+             
               style={{ border: 0 }}
+              className="w-full h-full"
               allowFullScreen
               loading="lazy"
               referrerPolicy="strict-origin-when-cross-origin"
